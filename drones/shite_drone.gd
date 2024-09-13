ff518793e49e76e42641c07823d6854debab1eaf
Thomas McDonald <naynaytom3000@gmail.com>
2024-09-12 23:04:10 -0400

    Drone can now emit pulse to detect player
    Added lots of sound affects

--- drones/shite_drone.gd
+++ drones/shite_drone.gd
@@ -1,15 +1,52 @@
 extends CharacterBody3D
 
-@export var nav_agent: NavigationAgent3D
 
 var player: CharacterBody3D
 
-func _ready() -> void:
-	$AnimationPlayer.play("oscilate")
+@export var detection_pulse_range = 30 # metres
+@export var detection_pulse_speed = 3 # time it takes to travel d_p_r distance
+var delay_before_pulse := 1.49 # sec
 
-func _process(delta: float) -> void:
-	velocity = Vector3.ZERO
-	if player != null:
-		nav_agent.set_target_position(player.global_position)
-		var next_nav_point = nav_agent.get_next_path_position()
-		velocity = (next_nav_point - global_position).normalized()
+var is_hunting = false
+
+func _ready() -> void:
+	oscilate()
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("debug_key"):
+		emit_detection_pulse()
+
+
+func oscilate():
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BACK)
+	var initial_pos =global_position.y
+	tween.tween_property(self, "position:y", initial_pos - 0.4, 1.5)
+	tween.tween_property(self, "position:y", initial_pos, 1.5)
+	tween.tween_callback(oscilate)
+
+func emit_detection_pulse():
+	$DetectionPulse.scale = Vector3(1,1,1)
+	$DetectionPulse.mesh.material.albedo_color = Color("9cffff58")
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)
+	var scaling_ratio = detection_pulse_range/$DetectionPulse.mesh.radius
+	$AudioStreamPlayer.play()
+	tween.tween_interval(delay_before_pulse)
+	tween.tween_property($DetectionPulse, "scale", Vector3(scaling_ratio, scaling_ratio, scaling_ratio), detection_pulse_speed).from_current()
+	tween.set_parallel()
+	tween.tween_property($DetectionPulse.mesh.material, "albedo_color", Color("9cffff00"), detection_pulse_speed)
+
+func _physics_process(delta: float) -> void:
+	if is_hunting:
+		velocity = (player.global_position - global_position).normalized() * 8
+		look_at(player.position)
+		move_and_slide()
+
+func start_hunting():
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.tween_property(self, "basis", Basis.looking_at(global_position.direction_to(player.position)), 1)
+	tween.tween_callback(func(): is_hunting = true)
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == "Character":
+		player = body
+		start_hunting()
--- world.gd
+++ world.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-func _ready() -> void:
-	for child in $NavigationRegion3D.get_children():
-		if child.is_in_group("player_hunter"):
-			child.player = $Character
+#func _ready() -> void:
+	#for child in get_children():
+		#if child.is_in_group("player_hunter"):
+			#child.player = $Character
